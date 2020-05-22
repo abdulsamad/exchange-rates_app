5cb05dce8491669a18820097a6b66bcc106f98ff
Abdul Samad <Samk8778@yahoo.com>
2020-05-23 02:19:37 +0530

    :construction: Added converter component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Nav from './components/layout/Navbar';
-import Converter from './components/converter/Converter';
+import Converter from './components/Converter';
 import './scss/App.scss';
 
 function App() {
 	return (
 		<div className='app'>
 			<Nav />
-			<div className='container-fluid px-2'>
-				<Row className='my-3'>
-					<Col md={4}>
+			<div className='container-fluid p-4 text-white'>
+				<Row className='mt-3 mb-5'>
+					<Col lg={1}></Col>
+					<Col lg={4} className='px-5'>
 						<Converter />
 					</Col>
 				</Row>
+				<br />
 			</div>
 		</div>
 	);
